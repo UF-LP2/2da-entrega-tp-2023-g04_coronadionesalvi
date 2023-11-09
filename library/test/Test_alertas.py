2b3deb9fac830761918320b17ac0bbc9d171d2c7
isadione <isadi@LAPTOP-A6R0FKV3>
2023-11-09 19:26:46 -0300

    interfaz

---
+++ library/test/Test_alertas.py
@@ -0,0 +1,6 @@
+import pytest
+from Hospital  import cHospital 
+from Paciente import cPaciente 
+from Medico import cMedico
+from Enfermero import cEnfermero 
+
--- main.py
+++ main.py
@@ -6,6 +6,12 @@ from library.Paciente import cPaciente
 
 
 def main() -> None:
+	pacientes = [paciente,paciente2,paciente3,paciente4]
+	enfermeros = [enfermero,enfermero2,enfermero3,enfermero4]
+	medicos = [medico,medico1,medico2,medico3]
+	
+	hospital = cHospital("Curitas","Vicente Fidel Lopez 543, caballito",pacientes,medicos,enfermeros)
+
 	enfermero = cEnfermero(43567555,"noche",False,False)
 	enfermero2 = cEnfermero(23765654,"tarde",False,False)
 	enfermero3 = cEnfermero(23223456,"maniana",True, False)
@@ -14,12 +20,12 @@ def main() -> None:
 	medico = cMedico(3254365)
 	medico1 = cMedico(4325211)
 	medico2 = cMedico(123513)
-	medico4 = cMedico(324652)
+	medico3 = cMedico(324652)
 
-	paciente = cPaciente(44787654,"verde",120,"en espera","15:30","esguince","16:30",3)
-	paciente2 = cPaciente(44787654,"verde",120,"en espera","15:30","esguince","16:30",3)
-	paciente3 = cPaciente(44787654,"verde",120,"en espera","15:30","esguince","16:30",3)
-	paciente4 = cPaciente(44787654,"verde",120,"en espera","15:30","esguince","16:30",3)
+	paciente = cPaciente(44787654,"azul",240,"en espera","08:30","sin urgencia","09:30",4)
+	paciente2 = cPaciente(22787654,"verde",120,"en espera","15:00","esguince","16:30",3)
+	paciente3 = cPaciente(44743223,"amarillo",60,"en espera","12:30","paresia","12:45",2)
+	paciente4 = cPaciente(22987359,"rojo",0,"atendido","15:30","politraumatismo","15:30",0)
 
 	enfermero.set_estado()
 
